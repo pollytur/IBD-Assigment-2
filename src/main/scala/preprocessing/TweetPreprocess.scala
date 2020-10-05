667f12e18908c804b8a129f7c78c8d1624408789
Andrey862 <f_a_m@list.ru>
2020-10-05 22:59:01 +0300

    upd preprocessing

--- src/main/scala/preprocessing/TweetPreprocess.scala
+++ src/main/scala/preprocessing/TweetPreprocess.scala
@@ -2,11 +2,7 @@ package preprocessing
 
 object TweetPreprocess {
 
-  def preprocessTweet(tweet: String): String = {
-    /*
-    removes user tags, replaces smiles with happy/sad tags, decrypts abbreviation
-    */
-
+  def dictRemoval(tweet: String): String = {
     var tweetLower = tweet.toLowerCase()
     //    https://stackoverflow.com/questions/40517562/how-to-implement-chain-of-string-replaceall-in-scala
 
@@ -21,16 +17,37 @@ object TweetPreprocess {
     tweetLower = ReplaceDictionaries.short2Normal.keys.foldLeft(tweetLower) { case (res, pattern) =>
       res.replaceAll(s"[^a-zA-Z]$pattern[^a-zA-Z]", " "+ReplaceDictionaries.short2Normal(pattern)+" ")
     }
-    //    removes all punctuation & numbers except - sign
-    var isUrl="""^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$"""
+    tweetLower
+  }
 
-    tweetLower.replaceAll("""[\p{Punct}&&[^-@]]""", " ")
+  def remUrl(tweet: String): String = {
+    var isUrl="""^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$"""
+    tweet
+    .split(" ")
+    .filter(x => !x.matches(isUrl))
+    .mkString(" ")
+  }
+
+  def remPunct(tweet: String): String = {
+    tweet
+    .replaceAll("""[\p{Punct}&&[^-@]]""", " ")
     .filter(!_.isDigit)
     .replaceAll(" +", " ")
+  }
+
+  def stopWords(tweet: String): String = {
+    tweet
     .split(" ")
     .filter(x => !x.contains("@"))
-    .filter(x => !x.matches(isUrl))
     .filter(x => !StopWords.stopWords.contains(x))
     .mkString(" ")
+  }
+
+  def preprocessTweet(tweet: String): String = {
+    /*
+    removes user tags, replaces smiles with happy/sad tags, decrypts abbreviation
+    */
+    //    removes all punctuation & numbers except - sign
+    stopWords(remPunct(remUrl(dictRemoval(tweet))))
   }
 }
