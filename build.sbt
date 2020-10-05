da6845fa086b11701f90250e1d7f2d6d2467cd5a
PollyTur <p.turischeva@innopolis.university>
2020-10-05 19:59:46 +0300

    sbtt setup

--- build.sbt
+++ build.sbt
@@ -4,4 +4,7 @@ version := "0.1"
 
 scalaVersion := "2.12.6"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "3.0.1"
+libraryDependencies ++=Seq( 
+  "org.apache.spark" %% "spark-core" % "3.0.1", 
+  "org.apache.spark" %% "spark-sql" % "3.0.1"
+)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,11 @@
+import org.apache.spark.sql.SparkSession
 import preprocessing.TweetPreprocess.preprocessTweet
 
 object Main extends App {
-  println(preprocessTweet("@model i love8 u take with u all the meu in 6291 :) "))
+  //  println(preprocessTweet("@model i love8 u take with u all the meu in 6291 :) "))
+  val session = SparkSession.builder().appName("app_name").master("local").getOrCreate()
+  println("done")
+  val training = session.read.format("csv").load("src/main/sourses/training.csv").rdd
+  println("done")
+  println(training.id)
 }
