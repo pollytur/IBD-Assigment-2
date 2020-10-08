a81518c3aa8a6db3acd6d948b8385461ee1e61ea
PollyTur <p.turischeva@innopolis.university>
2020-10-08 20:42:40 +0300

    updated main for new model

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,5 @@
-import org.apache.spark.ml.classification.LogisticRegressionModel
+import model.Train
+import org.apache.spark.ml.classification.{LinearSVCModel, LogisticRegressionModel}
 import org.apache.spark.ml.feature.Word2VecModel
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.functions.{col, udf}
@@ -9,12 +10,11 @@ import streaming.Streamer
 
 
 object Main extends App {
-
   val session = SparkSession.builder().appName("app_name").master("local[2]").getOrCreate()
   val sqlContext = session.sqlContext
 
-  val vectorizer = Word2VecModel.load("myWord2Vec")
-  val model = LogisticRegressionModel.load("myLR")
+  val vectorizer = Word2VecModel.load("myWord2Vec-100")
+  val model = LinearSVCModel.load("LinearSVC-100")
 
   val (stream, context) = Streamer.stream(session.sparkContext)
 
@@ -23,18 +23,18 @@ object Main extends App {
     arg.mkString(" ")
   }
   val trans = udf(coder2)
+  val preprocess : String => Seq[String] = (str: String) => {TweetPreprocess.preprocessTweet(str).split(" ")}
+  val processTweet = udf(preprocess)
 
   stream.foreachRDD(rdd => {
     import session.sqlContext.implicits._
 
     val result = rdd
-      .map((str: String) => TweetPreprocess.preprocessTweet(str).split(" "))
-      .filter(_.length > 0)
-      .toDF("textTransformed")
+      .toDF("initialTweet")
+      .withColumn("textTransformed", processTweet(col("initialTweet")))
       .transform((value: Dataset[Row]) => vectorizer.transform(value))
       .transform((value: Dataset[Row]) => model.transform(value))
-      .transform((value: Dataset[Row]) => value.select("textTransformed", "prediction"))
-      .withColumn("textTransformed", trans(col("textTransformed")))
+      .transform((value: Dataset[Row]) => value.select("initialTweet", "prediction"))
       .filter((row: Row) => row.getAs[String](0).length > 0)
 
     if (!result.rdd.isEmpty()) {
@@ -50,7 +50,7 @@ object Main extends App {
 
   def createDfForTweet(tweet: String): DataFrame = {
     session.createDataFrame(Seq(
-      tweet.split(" ")
+      tweet
     ).map(Tuple1.apply)).toDF("textTransformed")
   }
 }
